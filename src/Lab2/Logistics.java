ef665e40a6a6339763e46603d81fe8d494c4adab
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2016-02-10 19:14:41 +0100

    Lab2 almost finished, need to add more constraints

--- src/Lab2/Logistics.java
+++ src/Lab2/Logistics.java
@@ -1,9 +1,5 @@
 import org.jacop.core.Store;
 import org.jacop.core.IntVar;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-
 import org.jacop.constraints.*;
 import org.jacop.search.*;
 
@@ -11,110 +7,147 @@ public class Logistics {
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
 
-		totalDistance(2);
+//		System.out.println("\n*** Expected result: 20");
+//		int graph_size = 6;
+//		int start = 1;
+//		int n_dests = 1;
+//		int[] dest = { 6 };
+//		int n_edges = 7;
+//		int[] from = { 1, 1, 2, 2, 3, 4, 4 };
+//		int[] to = { 2, 3, 3, 4, 5, 5, 6 };
+//		int[] cost = { 4, 2, 5, 10, 3, 4, 11 };
+//		optimalDistance(graph_size, start, n_dests, dest, n_edges, from, to, cost);
+
+//		System.out.println("\n*** Expected result: 20");
+//		int graph_size = 6;
+//		int start = 1;
+//		int n_dests = 2;
+//		int[] dest = { 5, 6 };
+//		int n_edges = 7;
+//		int[] from = { 1, 1, 2, 2, 3, 4, 4 };
+//		int[] to = { 2, 3, 3, 4, 5, 5, 6 };
+//		int[] cost = { 4, 2, 5, 10, 3, 4, 11 };
+//		optimalDistance(graph_size, start, n_dests, dest, n_edges, from, to, cost);
+
+		System.out.println("\n*** Expected result: 11");
+		int graph_size = 6;
+		int start = 1;
+		int n_dests = 2;
+		int[] dest = { 5, 6 };
+		int n_edges = 9;
+		int[] from = { 1, 1, 1, 2, 2, 3, 3, 3, 4 };
+		int[] to = { 2, 3, 4, 3, 5, 4, 5, 6, 6 };
+		int[] cost = { 6, 1, 5, 5, 3, 5, 6, 4, 2 };
+		optimalDistance(graph_size, start, n_dests, dest, n_edges, from, to, cost);
 
 		long endTime = System.currentTimeMillis();
 		System.out.println("\n*** Execution time: " + (endTime - startTime) + " ms");
 
 	}
 
-	private static void calculateDistance(int graph_size, int start, int n_dests, int[] dest, int n_edges, int[] from,
+	@SuppressWarnings("deprecation")
+	private static void optimalDistance(int graph_size, int start, int n_dests, int[] dest, int n_edges, int[] from,
 			int[] to, int[] cost) {
+		/**
+		 * Initialize variables
+		 */
 		Store store = new Store();
 
-		/**
-		 * Initialize the variables
-		 */
+		IntVar[][] paths = new IntVar[n_dests][graph_size];
+		int[][] distances = new int[graph_size][graph_size];
+
 		final IntVar ZERO = new IntVar(store, "ZERO", 0, 0);
 		final IntVar ONE = new IntVar(store, "ONE", 1, 1);
 
-		IntVar[] paths = new IntVar[n_edges];
-
-		for (int i = 0; i < n_edges; i++) {
-			paths[i] = new IntVar(store, "path[" + i + "]", 0, 1);
+		/**
+		 * Populate all matrices
+		 */
+		for (int i = 0; i < n_dests; i++) {
+			for (int j = 0; j < graph_size; j++) {
+				paths[i][j] = new IntVar(store, "paths[" + i + "," + j + "]", 0, graph_size);
+			}
 		}
+
+		for (int i = 0; i < graph_size; i++) {
+			for (int j = 0; j < graph_size; j++) {
+				for (int k = 0; k < cost.length; k++) {
+					if ((i + 1 == from[k] && j + 1 == to[k]) || (i + 1 == to[k] && j + 1 == from[k])) {
+						distances[i][j] = cost[k];
+					} else {
+						distances[i][j] = 0;
+					}
+
+				}
+			}
+		}
+
 		/**
 		 * Add constraints
 		 */
-
-		// Must take atleast one path
-		store.impose(new SumInt(store, paths, "!=", ZERO));
-
-		// Must go to a destination atleast once
-		for (int d : dest) {
-			IntVar[] indexes = new IntVar[n_edges];
-
-			for (int i = 0; i < n_edges; i++) {
-				if (to[i] == d) {
-					indexes[i] = new IntVar(store, "res1." + i, 0, 1);
-				} else {
-					indexes[i] = ZERO;
-				}
-			}
-			store.impose(new SumInt(store, indexes, "!=", ZERO));
-
-			// TODO: rekursivt, kolla vilken edge den kmr ifr�n och sen k�ra
-			// samma sak p� n�sta edges, tills man kommer till start
+		// Constraint: make each row a sub-circuit (there must be a path to each destination)
+		for (int i = 0; i < n_dests; i++) {
+			store.impose(new Subcircuit(paths[i]));
 		}
 
-		// Base search on costs to minimize
-		IntVar[] costs = new IntVar[n_edges];
-		for (int c : cost) {
-
-		}
+		// Constraint: minimize by considering which paths were taken and what their cost was
+		IntVar distance = new IntVar(store, "cost", 0, sumArray(cost));
+		store.impose(new SumWeight(toIntVarArray(paths), toIntArray(distances), distance));
 
 		/**
 		 * Search & print solution
 		 */
-		System.out.println(
-				"Number of variables: " + store.size() + "\nNumber of constraints: " + store.numberConstraints());
-		Search<IntVar> search = new DepthFirstSearch<IntVar>();
-		SelectChoicePoint<IntVar> select = new SimpleSelect<IntVar>(paths, null, new IndomainMin<IntVar>());
-		search.setSolutionListener(new PrintOutListener<IntVar>());
-		if (search.labeling(store, select)) {
-			System.out.println("\n*** Yes");
-			System.out.println("Solution : " + java.util.Arrays.asList(paths));
+		System.out.println("Number of variables: " + store.size() +
+				"\nNumber of constraints: " + store.numberConstraints());
+
+		Search<IntVar> search = new DepthFirstSearch<>();
+		SelectChoicePoint<IntVar> select = new SimpleMatrixSelect<>(paths, null, new IndomainMin<>());
+
+		if (search.labeling(store, select, distance)) {
+			System.out.println("\n*** Found solution.");
+			// Print path
 		} else {
-			System.out.println("\n*** No");
+			System.out.println("\n*** No solution found.");
 		}
 	}
 
-	private static void totalDistance(int n) {
-		if (n == 1) {
-			System.out.println("\n*** Expected result: 20");
-			int graph_size = 6;
-			int start = 1;
-			int n_dests = 1;
-			int[] dest = { 6 };
-			int n_edges = 7;
-			int[] from = { 1, 1, 2, 2, 3, 4, 4 };
-			int[] to = { 2, 3, 3, 4, 5, 5, 6 };
-			int[] cost = { 4, 2, 5, 10, 3, 4, 11 };
-			calculateDistance(graph_size, start, n_dests, dest, n_edges, from, to, cost);
-		} else if (n == 2) {
-			System.out.println("\n*** Expected result: 20");
-			int graph_size = 6;
-			int start = 1;
-			int n_dests = 2;
-			int[] dest = { 5, 6 };
-			int n_edges = 7;
-			int[] from = { 1, 1, 2, 2, 3, 4, 4 };
-			int[] to = { 2, 3, 3, 4, 5, 5, 6 };
-			int[] cost = { 4, 2, 5, 10, 3, 4, 11 };
-			calculateDistance(graph_size, start, n_dests, dest, n_edges, from, to, cost);
-		} else if (n == 3) {
-			System.out.println("\n*** Expected result: 11");
-			int graph_size = 6;
-			int start = 1;
-			int n_dests = 2;
-			int[] dest = { 5, 6 };
-			int n_edges = 9;
-			int[] from = { 1, 1, 1, 2, 2, 3, 3, 3, 4 };
-			int[] to = { 2, 3, 4, 3, 5, 4, 5, 6, 6 };
-			int[] cost = { 6, 1, 5, 5, 3, 5, 6, 4, 2 };
-			calculateDistance(graph_size, start, n_dests, dest, n_edges, from, to, cost);
-		} else {
-			System.out.println("\n*** Invalid n");
+	/**
+	 * Gets the sum of an int array
+	 */
+	private static int sumArray(int[] array) {
+		int sum = 0;
+		for (int var : array) {
+			sum += var;
 		}
+		return sum;
+	}
+
+	/**
+	 * Takes all values from a matrix and inserts into an array
+     */
+	private static int[] toIntArray(int[][] matrix) {
+		int[] array = new int[matrix.length * matrix[0].length];
+		int index = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				array[index] = matrix[i][j];
+				index++;
+			}
+		}
+		return array;
+	}
+
+	/**
+	 * Takes all values from a matrix and inserts into an array
+	 */
+	private static IntVar[] toIntVarArray(IntVar[][] matrix) {
+		IntVar[] array = new IntVar[matrix.length * matrix[0].length];
+		int index = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				array[index] = matrix[i][j];
+				index++;
+			}
+		}
+		return array;
 	}
 }
